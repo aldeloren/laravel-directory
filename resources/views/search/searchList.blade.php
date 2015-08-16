9217126c579e21de15425fc38938d8edb379a8fc
Anthony DeLorenzo <a.ldeloren@gmail.com>
2015-08-16 15:52:23 -0400

    finishing search logic

--- resources/views/search/searchList.blade.php
+++ resources/views/search/searchList.blade.php
@@ -1,14 +1,29 @@
 @extends('layout')  
 
 @section('content')
-  @if ( $results )
-  @foreach ($data as $individual)
-    @if ( isset($individual['uid']) )
-      {{ $individual['cn'] }} <br/>
-    @endif 
-  @endforeach
 
-  @else
-    No Results Found.
+  @if ( $search_term && $fuzzy_search )
+    <p>Your search yielded no results. Searching instead for '{{ $search_term }}' garnered 
+    @if ( count($data) >= 100 )
+      {{ count($data) }} results, and was limited by the directory application. Please try to refine your search to find the appropriate party.</p>
+    @elseif ( count($data) < 100 )
+      {{ count($data) }} results.</p>
+    @endif
+  @elseif ( $search_term && !$fuzzy_search )
+    @if ( count($data) >= 100)
+      <p>Your search for '{{ $search_term }}' yielded {{ count($data) }} results and was limited by the directory application. Please try to refine your search to find the appropriate party.</p>
+    @else
+      <p>Your search for '{{ $search_term }}' yielded {{ count($data) }} results.</p>
+    @endif
   @endif
+
+  @if ( $results )
+    @foreach ($data as $individual)
+      @if ( isset($individual['uid']) )
+        {{ $individual['cn'] }} <br/>
+        {{ $individual['uid'] }} <br />
+      @endif 
+    @endforeach
+  @endif
+
 @stop
